ab2f123edc20a6d4fe1cb95aae320b955b63ad90
shenmi <13160920449@163.com>
2025-10-31 13:26:14 +0800

    fix 更改了element的配置

--- FE/kfc_frontend/src/main.js
+++ FE/kfc_frontend/src/main.js
@@ -5,6 +5,11 @@ import App from './App.vue'
 // 引入Element Plus和样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+// 引入Element Plus和样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 // 引入Element图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
