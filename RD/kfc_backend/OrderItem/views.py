d9b011531616f90e4929596339c3b218e42621b1
s1mple-114 <202741787@qq.com>
2025-11-02 00:25:05 +0800

    docs:为orderitem视图增添了备注

--- RD/kfc_backend/OrderItem/views.py
+++ RD/kfc_backend/OrderItem/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import orderitem
-from .serializers import orderitemSerializer
+from .models import orderitem  # 保持类名为驼峰命名
+from .serializers import orderitemSerializer  # 保持类名为驼峰命名
 
-class orderitemViewSet(viewsets.ModelViewSet):
+class orderitemViewSet(viewsets.ModelViewSet):  # 保持类名为驼峰命名
+    queryset = orderitem.objects.all()
     serializer_class = orderitemSerializer
     permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         return orderitem.objects.filter(order__user=self.request.user)
-# Create your views here.
