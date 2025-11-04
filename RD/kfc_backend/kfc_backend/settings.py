8149bb8c94aeb6c2af896807ff7a4e0214eb8a17
s1mple-114 <202741787@qq.com>
2025-11-04 18:27:11 +0800

    feat: 修改REST框架权限设置为全局登录认证

--- RD/kfc_backend/kfc_backend/settings.py
+++ RD/kfc_backend/kfc_backend/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication', # Token  认证
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated', # 全局登录认证
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
