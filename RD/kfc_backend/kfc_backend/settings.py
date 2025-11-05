12b399de7554db82849074848fa7aff9a49d6ebd
s1mple-114 <202741787@qq.com>
2025-11-05 23:35:00 +0800

    fix: 更新REST框架配置，禁用会话认证并要求身份验证

--- RD/kfc_backend/kfc_backend/settings.py
+++ RD/kfc_backend/kfc_backend/settings.py
@@ -139,11 +139,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',# Session  认证
+        # 'rest_framework.authentication.SessionAuthentication',# Session  认证
         'rest_framework.authentication.TokenAuthentication', # Token  认证
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
