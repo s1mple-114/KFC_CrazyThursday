e29f86a59be906136f1afe6565f758f3046cc4c4
s1mple-114 <202741787@qq.com>
2025-11-08 10:43:57 +0800

    fix: 更新静态文件目录配置

--- RD/kfc_backend/kfc_backend/settings.py
+++ RD/kfc_backend/kfc_backend/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# 如果需要自定义静态文件目录，请确保目录存在
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
