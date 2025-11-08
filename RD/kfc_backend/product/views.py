7256399d57409f4625ebbafea4192197b887cfb1
s1mple-114 <202741787@qq.com>
2025-11-08 10:44:08 +0800

    fix: 添加缓存控制头以禁止浏览器缓存响应

--- RD/kfc_backend/product/views.py
+++ RD/kfc_backend/product/views.py
@@ -24,7 +24,12 @@ class ProductViewSet(mixins.ListModelMixin,
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        # 添加缓存控制头，禁止浏览器缓存响应
+        response = Response(serializer.data)
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
     
     def get_queryset(self):
         queryset = Product.objects.filter(is_available=True)
